540f144c5ea6ae3a395dd695336a4c4ffaedf99b
max <max.assis@chatguru.com.br>
2024-03-22 14:51:40 -0300

    fazendo barra de input

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   SafeAreaView,
   Text,
@@ -5,6 +6,7 @@ import {
   View,
   FlatList,
   TextInput,
+  KeyboardAvoidingView,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
@@ -16,6 +18,7 @@ import { FlashList } from "@shopify/flash-list";
 import { chats } from "../../mock-chats";
 import Plus from "../../assets/plus.svg";
 import Mic from "../../assets/mic.svg";
+import EmojiPicker, { pt } from "rn-emoji-keyboard";
 
 interface chat {
   id: number;
@@ -23,74 +26,93 @@ interface chat {
   received: null | string;
 }
 
+
+
 export default function Chat() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [userInput, setUserInput] = useState("");
   const data = chats;
+
+  const handlePick = ({ emoji } : { emoji: string }) => {
+    setUserInput((text) => text + emoji);
+  };
 
   return (
     <SafeAreaView className="flex-1">
-      <View className="h-16 bg-cg-black items-center px-3 flex-row ">
-        <Ionicons name="arrow-back" size={35} color="#fff" />
+      <KeyboardAvoidingView behavior="padding" enabled className="flex-1">
+        <View className="h-16 bg-cg-black items-center px-3 flex-row ">
+          <Ionicons name="arrow-back" size={35} color="#fff" />
 
-        <Avatar className="h-12 w-12 ml-2">
-          <AvatarImage source={require("../../assets/compress2.jpg")} />
-          <AvatarFallback>CG</AvatarFallback>
-        </Avatar>
+          <Avatar className="h-12 w-12 ml-2">
+            <AvatarImage source={require("../../assets/compress2.jpg")} />
+            <AvatarFallback>CG</AvatarFallback>
+          </Avatar>
 
-        <View className="h-12 w-56 ml-2 mb-1">
-          <Text
-            numberOfLines={1}
-            className="text-white text-lg font-roboto-bold"
-          >
-            Gabi Ware
-          </Text>
+          <View className="h-12 w-56 ml-2 mb-1">
+            <Text
+              numberOfLines={1}
+              className="text-white text-lg font-roboto-bold"
+            >
+              Gabi Ware
+            </Text>
 
-          <View className={PillStyles({ intent: "AGUARDANDO" })}>
-            <Text className="text-white">AGUARDANDO</Text>
+            <View className={PillStyles({ intent: "AGUARDANDO" })}>
+              <Text className="text-white">AGUARDANDO</Text>
+            </View>
+          </View>
+
+          <TouchableOpacity className="ml-auto">
+            <Ionicons name="ellipsis-vertical-sharp" size={28} color="#fff" />
+          </TouchableOpacity>
+        </View>
+
+        <View className="flex-1">
+          <FlashList
+            overScrollMode="never"
+            inverted
+            data={[...chats].reverse()}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) =>
+              item.send ? (
+                <SendMsg msg={item.send!} />
+              ) : (
+                <ReceivedMsg msg={item.received!} />
+              )
+            }
+            estimatedItemSize={91}
+          />
+        </View>
+        
+        <EmojiPicker
+          onEmojiSelected={handlePick}
+          open={isOpen}
+          onClose={() => setIsOpen(false)}
+          translation={pt}
+          enableRecentlyUsed
+          categoryPosition="top"
+        />    
+
+        <View className="min-h-[70] max-h-[] py-3 max-h-15o px-3 border-t-[0.5px] border-slate-400 flex-row items-center">
+          <Plus width={40} height={40} />
+
+          <View className="flex-1 flex-row">
+            <TextInput
+              multiline={true}
+              selectionColor={"#14B67B"}
+              className="flex-1 min-h-12 ml-3 p-3 pr-0 bg-slate-100 rounded-tl-sm rounded-bl-sm "
+            ></TextInput>
+
+            <TouchableOpacity onPress={() => setIsOpen(true)} className="w-12 items-center justify-center bg-slate-100 mr-3 rounded-tr-sm rounded-br-sm i">
+              <Ionicons name="happy-outline" size={32} color="black" />
+            </TouchableOpacity>
+          </View>
+
+          <View className="w-12 h-12 bg-cg-green items-center justify-center">
+            {/* <Ionicons name="send" size={22} color="#fff" /> */}
+            <Mic />
           </View>
         </View>
-
-        <TouchableOpacity className="ml-auto">
-          <Ionicons name="ellipsis-vertical-sharp" size={28} color="#fff" />
-        </TouchableOpacity>
-      </View>
-
-      <View className="flex-1">
-        <FlashList
-          overScrollMode="never"
-          inverted
-          data={[...chats].reverse()}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) =>
-            item.send ? (
-              <SendMsg msg={item.send!} />
-            ) : (
-              <ReceivedMsg msg={item.received!} />
-            )
-          }
-          estimatedItemSize={91}
-        />
-      </View>
-
-      <View className="min-h-[70] max-h-[] py-3 max-h-15o px-3 border-t-[0.5px] border-slate-400 flex-row items-center">
-        <Plus width={40} height={40} />
-
-        <View className="flex-1 flex-row">
-          <TextInput
-            multiline={true}
-            selectionColor={"#14B67B"}
-            className="flex-1 min-h-12 ml-3 p-3 pr-0 bg-slate-100 rounded-tl-sm rounded-bl-sm "
-          ></TextInput>
-
-          <View className="w-12 items-center justify-center bg-slate-100 mr-3 rounded-tr-sm rounded-br-sm i">
-            <Ionicons name="happy-outline" size={32} color="black" />
-          </View>
-        </View>
-
-        <View className="w-12 h-12 bg-cg-green items-center justify-center">
-          {/* <Ionicons name="send" size={22} color="#fff" /> */}
-          <Mic />
-        </View>
-      </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
