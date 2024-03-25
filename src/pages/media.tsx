b6dfe0a172ee0786b5eb4c2363517742cde9a9f0
max <max.assis@chatguru.com.br>
2024-03-25 19:48:27 -0300

    feito pagina de midia

--- src/pages/media.tsx
+++ src/pages/media.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
-import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView, Text, TouchableOpacity, View, ScrollView  } from "react-native";
 import HeaderMenu from "../components/HeaderMenu";
 import { AntDesign } from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
 import { cva } from "class-variance-authority";
+import { Ionicons } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import { SimpleLineIcons } from '@expo/vector-icons';
 
 export default function Media() {
     const [media, setMedia] = useState(true)
@@ -43,7 +46,7 @@ export default function Media() {
           </View>
         </View>
 
-        <View className="h-[38px] bg-cg-green mx-5 rounded-md flex-row">
+        <View className="h-[38px] bg-cg-green mx-5 rounded-md mb-5 flex-row">
           <TouchableOpacity onPress={selectMedia} className="w-4/12 h-full items-center justify-center">
             <View className={SelectStyles({ intent: media })}>
               <Text className={TextSelect({ intent: media })}>Mídias</Text>
@@ -62,6 +65,87 @@ export default function Media() {
             </View>
           </TouchableOpacity>       
         </View>
+
+        <ScrollView>
+            <View className="h-[72px] mx-5 mb-3 px-5 border-[1px] border-slate-300 rounded-md justify-center">
+                <View className="h-9 flex-row justify-between">
+                    <View className="flex-row gap-x-4">
+                        <View className="h-10 w-10 rounded-full bg-green-100 items-center justify-center">
+                            <Ionicons name="image-outline" size={26} color={colors.green[500]} />
+                        </View>
+
+                        <View>
+                            <Text className="font-roboto-bold">Nome do arquivo</Text>
+                            <Text>20/03/2024 as 23:00</Text>
+                        </View>
+                    </View>
+
+                    <View className="flex-row gap-x-2">
+                        <View className="h-9 w-9 bg-slate-300 items-center justify-center">
+                            <AntDesign name="download" size={20} color={colors.slate[500]} />
+                        </View>
+                        <View className="h-9 w-9 bg-slate-300 items-center justify-center">
+                            <Feather name="send" size={20} color={colors.slate[500]} />
+                        </View>
+                    </View>
+                </View>
+         
+            </View>
+
+            <View className="h-[72px] mx-5 mb-3 px-5 border-[1px] border-slate-300 rounded-md justify-center">
+                <View className="h-9 flex-row justify-between">
+                    <View className="flex-row gap-x-4">
+                        <View className="h-10 w-10 rounded-full bg-red-100 items-center justify-center">
+                            {/* <Ionicons name="image-outline" size={26} color={colors.green[500]} /> */}
+                            <Ionicons name="document-text-outline" size={26} color={colors.red[500]} />
+                        </View>
+
+                        <View>
+                            <Text className="font-roboto-bold">Nome do arquivo</Text>
+                            <Text>20/03/2024 as 23:00</Text>
+                        </View>
+                    </View>
+
+                    <View className="flex-row gap-x-2">
+                        <View className="h-9 w-9 bg-slate-300 items-center justify-center">
+                            <AntDesign name="download" size={20} color={colors.slate[500]} />
+                        </View>
+                        <View className="h-9 w-9 bg-slate-300 items-center justify-center">
+                            <Feather name="send" size={20} color={colors.slate[500]} />
+                        </View>
+                    </View>
+                </View>
+         
+            </View>
+
+            <View className="h-[72px] mx-5 mb-3 px-5 border-[1px] border-slate-300 rounded-md justify-center">
+                <View className="h-9 flex-row justify-between">
+                    <View className="flex-row gap-x-4">
+                        <View className="h-10 w-10 rounded-full bg-blue-100 items-center justify-center">
+                            <SimpleLineIcons name="emotsmile" size={25} color={colors.blue[500]} />
+                        </View>
+
+                        <View>
+                            <Text className="font-roboto-bold">Nome do arquivo</Text>
+                            <Text>20/03/2024 as 23:00</Text>
+                        </View>
+                    </View>
+
+                    <View className="flex-row gap-x-2">
+                        <View className="h-9 w-9 bg-slate-300 items-center justify-center">
+                            <AntDesign name="download" size={20} color={colors.slate[500]} />
+                        </View>
+                        <View className="h-9 w-9 bg-slate-300 items-center justify-center">
+                            <Feather name="send" size={20} color={colors.slate[500]} />
+                        </View>
+                    </View>
+                </View>
+         
+            </View>
+        </ScrollView>
+
+
+
       </View>
     </SafeAreaView>
   );
