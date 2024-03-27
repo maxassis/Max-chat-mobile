04454cb1b60e2989ca6ea55396a92b00c8a98ea0
max <max.assis@chatguru.com.br>
2024-03-27 09:22:32 -0300

    fazendo pagina de informaçoes

--- src/pages/informations.tsx
+++ src/pages/informations.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { SafeAreaView, View, Text, KeyboardAvoidingView, TextInput } from "react-native";
+import { ScrollView, SafeAreaView, View, Text, KeyboardAvoidingView, TextInput } from "react-native";
 import HeaderMenu from "../components/HeaderMenu";
 import { CheckBox, Divider } from "@rneui/themed";
+import { AntDesign } from '@expo/vector-icons';
 
 export default function Informations() {
     const [checked, setChecked] = useState(true);
@@ -12,6 +13,7 @@ export default function Informations() {
             <KeyboardAvoidingView behavior="padding" enabled className="flex-1">
                 <HeaderMenu selected="informations" />
                 <View className="flex-1 mt-5">
+                   <ScrollView overScrollMode="never">
                     <Text className="text-xl ml-5 font-roboto-bold mb-5">Informações do chat</Text>
 
                     <Text className="ml-5 font-roboto-bold mb-1">Nome:</Text>
@@ -79,10 +81,84 @@ export default function Informations() {
                     </View>
 
                     <Divider />
+
+                    <View className="flex-row mx-5 mt-5 mb-5 items-center justify-between">           
+                            <Text className="text-xl font-roboto-bold">Tags</Text>
+                        <View className="bg-cg-green  rounded-sm h-6 w-6 items-center justify-center">
+                            <AntDesign name="plus" size={20} color="#fff" />
+                        </View>        
+                    </View> 
+
+                    <View className="mx-5 mb-5 flex-row flex-wrap gap-y-3 justify-around">
+                            <View className="bg-pink-200 self-start px-3 py-1 rounded-2xl">
+                                <Text>Nome da tag</Text>
+                            </View>
+
+                            <View className="bg-blue-200 self-start px-3 py-1 rounded-2xl">
+                                <Text>Nome da tag</Text>
+                            </View>
+
+                            <View className="bg-red-200 self-start px-3 py-1 rounded-2xl">
+                                <Text>Nome da tag</Text>
+                            </View>
+
+                            <View className="bg-green-200 self-start px-3 py-1 rounded-2xl">
+                                <Text>Nome da tag</Text>
+                            </View>
+
+                            <View className="bg-purple-200 self-start px-3 py-1 rounded-2xl">
+                                <Text>Nome da tag</Text>
+                            </View>
+
+                            <View className="bg-gray-200 self-start px-3 py-1 rounded-2xl">
+                                <Text>Nome da tag</Text>
+                            </View>
+
+                            <View className="bg-orange-200 self-start px-3 py-1 rounded-2xl">
+                                <Text>Nome da tag</Text>
+                            </View>
+                    </View>
+
+                    <Divider />
+
+                    <Text className="mt-5 ml-5 mb-5 text-xl font-roboto-bold">Campos personalizados</Text>
+                    <View className="mx-5 mb-3 flex-row justify-between items-center">
+                        <Text className="text-base h-9 text-slate-400">Nome da empresa:</Text>
+                        <TextInput className="border-[0.5px] w-6/12 border-slate-400 h-full rounded-sm" />    
+                    </View> 
+
+                    <View className="mx-5 mb-3 flex-row justify-between items-center">
+                        <Text className="text-base h-9 text-slate-400">Nome da empresa:</Text>
+                        <TextInput className="border-[0.5px] w-6/12 border-slate-400 h-full rounded-sm" />    
+                    </View> 
+
+                    <View className="mx-5 mb-3 flex-row justify-between items-center">
+                        <Text className="text-base h-9 text-slate-400">CNPJ:</Text>
+                        <TextInput className="border-[0.5px] w-6/12 border-slate-400 h-full rounded-sm" />    
+                    </View> 
+
+                    <View className="mx-5 mb-3 flex-row justify-between items-center">
+                        <Text className="text-base h-9 text-slate-400">E-mail:</Text>
+                        <TextInput className="border-[0.5px] w-6/12 border-slate-400 h-full rounded-sm" />    
+                    </View> 
+
+                    <View className="mx-5 mb-3 flex-row justify-between items-center">
+                        <Text className="text-base h-9 text-slate-400">Data de cadastro:</Text>
+                        <TextInput className="border-[0.5px] w-6/12 border-slate-400 h-full rounded-sm" />    
+                    </View> 
+
+                    <View className="mx-5 mb-3 flex-row justify-between items-center">
+                        <Text className="text-base h-9 text-slate-400">Data da conta:</Text>
+                        <TextInput className="border-[0.5px] w-6/12 border-slate-400 h-full rounded-sm" />    
+                    </View> 
+
+                    <View className="mx-5 mb-5 flex-row justify-between items-center">
+                        <Text className="text-base h-9 text-slate-400">Aparelho de origem:</Text>
+                        <TextInput className="border-[0.5px] w-6/12 border-slate-400 h-full rounded-sm" />    
+                    </View> 
+          
+                    </ScrollView>                
                 </View>
-
-                
-
             </KeyboardAvoidingView>
         </SafeAreaView>
 
